5ddc7fb133bc5f3e0611e8380aec0b97a3e80069
Ben Tupper <btupper@bigelow.org>
2020-08-06 11:02:54 -0400

    update code to allow for other buoys

--- R/buoy.R
+++ R/buoy.R
@@ -1,9 +1,11 @@
 #' Read in buoy data
 #'
 #' @export
-#' @seealso \href{http://gyre.umeoce.maine.edu/data/gomoos/buoy/html/M01.html}{UMaine Buoy Data}
-#' @param buoy the
-read_buoy <- function(buoy = "M01"){
+#' @seealso \href{http://gyre.umeoce.maine.edu/data/gomoos/buoy/html/M01.html}{UMaine Buoy M01 Data}
+#' @seealso \href{http://gyre.umeoce.maine.edu/data/gomoos/buoy/html/I01.html}{UMaine Buoy I01 Data}
+#' @param buoy the buoy to load - either "I01" or "M01"
+#' @return a data frame (tibble) of buoy data
+read_buoy <- function(buoy = c("I01","M01")[1]){
 
   filename <- system.file(file.path("buoy", paste0(buoy[1], "_sbe37_1m.csv.gz")),
                      package = "ohwobpg")
